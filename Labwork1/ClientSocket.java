377aaa03acf6eb3a1fd0c2cd02e28fe6719f3b99
hieuvdbi9102 <59214816+hieuvdbi9102@users.noreply.github.com>
2020-03-04 21:01:54 +0700

    Update ClientSocket.java

--- Labwork1/ClientSocket.java
+++ Labwork1/ClientSocket.java
@@ -1,1 +1,25 @@
+import java.io.*;
+import java.net.*;
 
+class ClientSocket{
+  
+ public static void main(String argv[]) throws Exception{
+  String messageSent;
+  String messageReceipt;
+   
+  BufferedReader userInput = new BufferedReader( new InputStreamReader(System.in));
+   
+  Socket clientSocket = new Socket("127.0.0.1", 8888);
+  DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream()); //tao luồng gửi đi
+  BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())); // tạo luồng nhận về
+   
+  messages = userInput.readLine();
+  outToServer.writeBytes(messages + '\n'); //gửi đi
+   
+  messageReceipt = inFromServer.readLine(); //nhận về từ server
+  System.out.println("FROM SERVER: " + messageReceipt);
+   
+  clientSocket.close();
+ }
+  
+}
