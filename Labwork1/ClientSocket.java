2519e6720bb6d53acffba0db8c57d0aa7fb9401f
hieuvdbi9102 <59214816+hieuvdbi9102@users.noreply.github.com>
2020-03-04 21:12:46 +0700

    Update ClientSocket.java

--- Labwork1/ClientSocket.java
+++ Labwork1/ClientSocket.java
@@ -7,16 +7,16 @@ class ClientSocket{
   String messageSent;
   String messageReceipt;
    
-  BufferedReader userInput = new BufferedReader( new InputStreamReader(System.in));
+  BufferedReader userInput = new BufferedReader( new InputStreamReader(System.in)); //Nhập liệu
    
-  Socket clientSocket = new Socket("127.0.0.1", 8888);
+  Socket clientSocket = new Socket("127.0.0.1", 8888); //Tạo socket client là localhost lắng nghe ở cổng 8888 
   DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream()); //tao luồng gửi đi
   BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())); // tạo luồng nhận về
    
   messages = userInput.readLine();
   outToServer.writeBytes(messages + '\n'); //gửi đi
    
-  messageReceipt = inFromServer.readLine(); //nhận về từ server
+  messageReceipt = inFromServer.readLine(); //nhận về từ server (text bị chuyển hết thành upercase ở server để dễ phân biệt )
   System.out.println("FROM SERVER: " + messageReceipt);
    
   clientSocket.close();
